2747d7947c428df5620e6a7b0375e2e098ac0627
yokki167 <60569769+yokki167@users.noreply.github.com>
2020-02-11 20:34:46 +0900

    add 非同期

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){
+    function buildHTML(message){
+      if ( message.image) {
+        var html = `<div class="chat-main__messages--message">
+        <div class="chat-main__messages--message--box">
+        <div class="chat-main__messages--message--box--name">
+        ${message.user_name}
+        </div>
+        <div class="chat-main__messages--message--box--day">
+        ${message.created_at}
+        </div>
+        </div>
+        <div class="chat-main__messages--message--text">
+        <p class="chat-main__messages--message--text--a">
+        ${message.text}
+        </p>
+        <img class="lower-message__image" src=${message.image} >
+        </div>
+        </div>`
+        return html;
+        
+      } else {
+        var html =
+        `<div class="chat-main__messages--message">
+        <div class="chat-main__messages--message--box">
+        <div class="chat-main__messages--message--box--name">
+        ${message.user_name}
+        </div>
+        <div class="chat-main__messages--message--box--day">
+        ${message.created_at}
+        </div>
+        </div>
+        <div class="chat-main__messages--message--text">
+        <p class="chat-main__messages--message--text--a">
+        ${message.text}
+        </p>
+        
+        </div>
+        </div>`
+        return html;
+      };
+    }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $('.submit-btn').removeAttr('data-disable-with');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log(data)
+      var html = buildHTML(data);
+      $('.chat-main__messages').append(html);
+      $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+      $('form')[0].reset();
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
+  })
+});
--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,13 @@
 require_relative 'boot'
 
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
