719f4411bcad88db377fac26e46ffffc1b82d0e5
yokki167 <60569769+yokki167@users.noreply.github.com>
2020-02-14 21:05:52 +0900

    非同期通信修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
     var buildHTML = function(message) {
       if ( message.text && message.image) {
         var html = `
-        <div class="message" data-message-id=${message.id}>
+        <div class="message" data-message-id="${message.id}">
           <div class="chat-main__messages--message">
             <div class="chat-main__messages--message--box">
               <div class="chat-main__messages--message--box--name">
@@ -22,7 +22,7 @@ $(function(){
         </div>`
       } else if (message.text) {
         var html =`
-        <div class="message" data-message-id=${message.id}>
+        <div class="message" data-message-id="${message.id}">
 
           <div class="chat-main__messages--message">
             <div class="chat-main__messages--message--box">
@@ -42,7 +42,7 @@ $(function(){
         </div>`
       } else if (message.image) {
         var html = `
-        <div class="message" data-message-id=${message.id}>
+        <div class="message" data-message-id="${message.id}">
 
           <div class="chat-main__messages--message">
             <div class="chat-main__messages--message--box">
@@ -63,7 +63,6 @@ $(function(){
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action');
-    $('.submit-btn').removeAttr('data-disable-with');
     $.ajax({
       url: url,
       type: "POST",
@@ -85,8 +84,7 @@ $(function(){
 
 
   var reloadMessages = function() {
-    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    last_message_id = $('.message:last').data("message-id");
+    last_message_id = $('.message:last').data('message-id');
     $.ajax({
       url: "api/messages",
       type: 'get',
@@ -94,19 +92,22 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      if (messages.length !== 0)
+      if (messages.length !== 0){
         var insertHTML = '';
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
         $('.chat-main__messages').append(insertHTML);
         $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+      }
     })
 
     .fail(function() {
       alert('エラーです');
     });
-    }
+    
   };
-  setInterval(reloadMessages, 7000)
-})
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+}
+});
